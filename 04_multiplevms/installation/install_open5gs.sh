cea1a72be47a3eeae9a91f28d0ebc63911701a9a
Mohit Kumar <mohitkr05@gmail.com>
2023-02-24 19:59:21 +1100

    added multiple vms

--- 04_multiplevms/installation/install_open5gs.sh
+++ 04_multiplevms/installation/install_open5gs.sh
@@ -6,7 +6,7 @@ wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc > key.asc
 sudo apt-key add key.asc
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 apt-get -y update
-apt install -y mongodb-org
+apt install -y mongodb-org mongodb-clients
 sudo systemctl start mongod
 sudo systemctl enable mongod
 add-apt-repository ppa:open5gs/latest
